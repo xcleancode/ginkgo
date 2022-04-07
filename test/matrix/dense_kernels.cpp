66a64f8032feb61f5b047db8a4ed7ce4b3dfd137
Aditya Kashi <aditya.kashi@kit.edu>
2022-04-07 11:00:58 +0200

    added dense add_scale

--- test/matrix/dense_kernels.cpp
+++ test/matrix/dense_kernels.cpp
@@ -952,6 +952,17 @@ TEST_F(Dense, SingleVectorAddScaledIsEquivalentToRef)
 }
 
 
+TEST_F(Dense, SingleVectorAddScaleIsEquivalentToRef)
+{
+    set_up_vector_data(1);
+
+    x->add_scale(alpha.get(), y.get(), beta.get());
+    dx->add_scale(dalpha.get(), dy.get(), dbeta.get());
+
+    GKO_ASSERT_MTX_NEAR(dx, x, r<value_type>::value);
+}
+
+
 TEST_F(Dense, SingleVectorSubtractScaledIsEquivalentToRef)
 {
     set_up_vector_data(1);
@@ -969,6 +980,17 @@ TEST_F(Dense, SingleVectorComplexAddScaledIsEquivalentToRef)
 
     c_x->add_scaled(c_alpha.get(), c_y.get());
     dc_x->add_scaled(dc_alpha.get(), dc_y.get());
+
+    GKO_ASSERT_MTX_NEAR(dc_x, c_x, r<value_type>::value);
+}
+
+
+TEST_F(Dense, SingleVectorComplexAddScaleIsEquivalentToRef)
+{
+    set_up_vector_data(1);
+
+    c_x->add_scale(c_alpha.get(), c_y.get(), c_beta.get());
+    dc_x->add_scale(dc_alpha.get(), dc_y.get(), dc_beta.get());
 
     GKO_ASSERT_MTX_NEAR(dc_x, c_x, r<value_type>::value);
 }
@@ -996,6 +1018,17 @@ TEST_F(Dense, SingleVectorComplexRealAddScaledIsEquivalentToRef)
 }
 
 
+TEST_F(Dense, SingleVectorComplexRealAddScaleIsEquivalentToRef)
+{
+    set_up_vector_data(1);
+
+    c_x->add_scale(alpha.get(), c_y.get(), beta.get());
+    dc_x->add_scale(dalpha.get(), dc_y.get(), dbeta.get());
+
+    GKO_ASSERT_MTX_NEAR(dc_x, c_x, r<value_type>::value);
+}
+
+
 TEST_F(Dense, SingleVectorComplexRealSubtractScaledIsEquivalentToRef)
 {
     set_up_vector_data(1);
@@ -1013,6 +1046,17 @@ TEST_F(Dense, MultipleVectorAddScaledIsEquivalentToRef)
 
     x->add_scaled(alpha.get(), y.get());
     dx->add_scaled(dalpha.get(), dy.get());
+
+    GKO_ASSERT_MTX_NEAR(dx, x, r<value_type>::value);
+}
+
+
+TEST_F(Dense, MultipleVectorAddScaleIsEquivalentToRef)
+{
+    set_up_vector_data(20);
+
+    x->add_scale(alpha.get(), y.get(), beta.get());
+    dx->add_scale(dalpha.get(), dy.get(), dbeta.get());
 
     GKO_ASSERT_MTX_NEAR(dx, x, r<value_type>::value);
 }
@@ -1079,6 +1123,17 @@ TEST_F(Dense, MultipleVectorAddScaledWithDifferentAlphaIsEquivalentToRef)
 
     x->add_scaled(alpha.get(), y.get());
     dx->add_scaled(dalpha.get(), dy.get());
+
+    GKO_ASSERT_MTX_NEAR(dx, x, r<value_type>::value);
+}
+
+
+TEST_F(Dense, MultipleVectorAddScaleWithDifferentAlphaIsEquivalentToRef)
+{
+    set_up_vector_data(20, true);
+
+    x->add_scale(alpha.get(), y.get(), beta.get());
+    dx->add_scale(dalpha.get(), dy.get(), dbeta.get());
 
     GKO_ASSERT_MTX_NEAR(dx, x, r<value_type>::value);
 }
